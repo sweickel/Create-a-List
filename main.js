a0cf81f1320791baa55789eff8b56ac4042345bc
sweickel <53339825+sweickel@users.noreply.github.com>
2019-08-26 00:31:27 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+
+var addItem = function() {
+	if (document.getElementById('input-item').textContent !== "") {
+		// add new html element with list item
+		console.log('Test Complete.')
+	} 
+}
+
+
+
+
+//Adds list item to list
+document.querySelector('.submit').addEventListener('click', addItem);
